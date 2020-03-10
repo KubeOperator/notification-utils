b71a9b92e678a634102e2087e7f53efa66f8eefd
wangzhengkun <zhengkun@fit2cloud.com>
2020-03-10 17:12:47 +0800

    init

--- notification_utils/notification_test.py
+++ notification_utils/notification_test.py
@@ -4,8 +4,8 @@ from notification_utils.ding_talk import DingTalk
 class NotificationTest(unittest.TestCase):
 
     def test_ding_talk(self):
-        ding_talk = DingTalk('0e960f1b44ed6e5f7cb5874e709cc24d6c37827de9a63ba1b18403f0a47ea87d')
-        success = ding_talk.send_message('13521797236','Warning','SEC047fd6938eec907fdbefa2120639a9a178d1ff769a674c4027e7d41f2ea3dd13')
+        ding_talk = DingTalk('toekn')
+        success = ding_talk.send_message('','message','')
         print(success)
 
 
